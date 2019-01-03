654dad223766b5754d4f8d2679c993b586edf4ed
Paul <43698605+chemistrytocode@users.noreply.github.com>
2019-01-03 09:51:26 +0000

    Create Rock Paper Scissors!.rb

---
+++ Rock Paper Scissors!.rb
@@ -0,0 +1,10 @@
+#Rock Paper Scissors
+#Let's play! You have to return which player won! In case of a draw return Draw!.
+
+def rps(p1, p2)
+  return "Draw!" if p1 == p2
+  return "Player 1 won!" if p1 == "rock" && p2 == "scissors"
+  return "Player 1 won!" if p1 == "scissors" && p2 == "paper"
+  return "Player 1 won!" if p1 == "paper" && p2 == "rock"
+  return "Player 2 won!"
+end
